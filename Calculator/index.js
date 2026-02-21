de71729b37254104226235a616d13dab37171319
Ansh-Gusain Github <anshgusain2005@gmail.com>
2026-02-21 13:06:21 +0530

    Calculator

---
+++ Calculator/index.js
@@ -0,0 +1,19 @@
+display=document.getElementById('display');
+
+function appendToDisplay(input){
+    display.value+=input;
+}
+function clearDisplay(){
+    display.value='';
+}
+function deleteLast(){
+    display.value=display.value.slice(0,-1);
+}
+function Calculate(){
+    try{
+        display.value=eval(display.value);
+    }
+    catch(error){
+        display.value='Error';
+    }
+}
